f567651c7dd2416f924e7d2cd94760c53e37e876
Thomas Henley <thomas.e.henley@gmail.com>
2025-10-05 20:55:45 -0400

    add first tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,43 @@
+package auth_test
+
+import (
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		headers http.Header
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name:    "empty header",
+			headers: http.Header{},
+			want:    "",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := auth.GetAPIKey(tt.headers)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("GetAPIKey() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("GetAPIKey() succeeded unexpectedly")
+			}
+			// TODO: update the condition below to compare got with tt.want.
+			if true {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
